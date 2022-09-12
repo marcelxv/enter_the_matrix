3b026aefb7c759356c5a62b18862da04d06ed404
Marcel Scognamiglio <67491786+marcelxv@users.noreply.github.com>
2022-09-12 09:41:28 -0300

    teste

--- scripts/script.js
+++ scripts/script.js
@@ -5,5 +5,5 @@ labels.forEach(label => {
     .split('') // transforma em array de letras
     .map((letter, idx) => `<span style="transition-delay:${idx * 100}ms">${letter}</span>`) // adiciona span
     .join(''); // transforma em string
-  }); 
-
+    // mais uma linha de código
+  });
